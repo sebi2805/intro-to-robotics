4096f988d77727803e2f39947132ae4aa5dfb5e1
Virtopeanu Sebastian <f_seby@yahoo.com>
2023-11-12 15:38:32 +0200

    added working version

--- Homeworks/Homework5_Stopwatch/sketch_nov9a/sketch_nov9a.ino
+++ Homeworks/Homework5_Stopwatch/sketch_nov9a/sketch_nov9a.ino
@@ -1,47 +1,57 @@
-const int numDigits = 4; // Number of digits in the display
 
 // Pin definitions for the buttons
 const int startPauseButtonPin = 2;
 const int saveLapButtonPin = 3;
 const int resetButtonPin = 8;
 
-// Pin definitions for the 74HC595 shift register
-const int dataPin = 12;  // Connect to DS of 74HC595
-const int latchPin = 11; // Connect to ST_CP of 74HC595
-const int clockPin = 10; // Connect to SH_CP of 74HC595
-
 // Variables for stopwatch logic
 volatile bool isRunning = false;
-unsigned long stopwatchTime = 0;
+
+unsigned long lastDebounceTimeStartPause = 0;
+unsigned long lastDebounceTimeSaveLap = 0;
+
 unsigned long lastUpdateTime = 0;
-const unsigned long updateInterval = 100; // Update interval in milliseconds (10th of a second)
 unsigned long lapTimes[4] = {0};
 int lapIndex = 0;
 
-// ISR for start/pause button
-volatile unsigned long lastDebounceTimeStartPause = 0;
-volatile unsigned long lastDebounceTimeSaveLap = 0;
-volatile unsigned long lastDebounceTimeReset = 0;
-const unsigned long debounceDelay = 100; // Debounce delay in milliseconds
+int displayNumber = 0;
+int number = 0;
+// Define connections to the shift register
+const int latchPin = 11; // Connects to STCP (latch pin) on the shift register
+const int clockPin = 10; // Connects to SHCP (clock pin) on the shift register
+const int dataPin = 12;  // Connects to DS (data pin) on the shift register
+// Define connections to the digit control pins for a 4-digit display
+const int segD1 = 4;
+const int segD2 = 5;
+const int segD3 = 6;
+const int segD4 = 7;
+// Store the digits in an array for easy access
+int displayDigits[] = {segD1, segD2, segD3, segD4};
+const int displayCount = 4; // Number of digits in the display
+// Define the number of unique encodings (0-9, A-F for hexadecimal)
+const int debounceDelay = 100;
 
-const int digitPins[4] = {4, 5, 6, 7};
+const int encodingsNumber = 16; // Variables for controlling the display update timing
+unsigned long lastIncrement = 0;
+unsigned long delayCount = 50; // Delay between updates (milliseconds)
+
+// Define byte encodings for the hexadecimal characters 0-F
+byte byteEncodings[encodingsNumber] = {
+    // A B C D E F G DP
+    B11111100, // 0
+    B01100000, // 1
+    B11011010, // 2
+    B11110010, // 3
+    B01100110, // 4
+    B10110110, // 5
+    B10111110, // 6
+    B11100000, // 7
+    B11111110, // 8
+    B11110110, // 9
+
+};
 
 // Function to turn off all digits
-void clearDigits()
-{
-  for (int i = 0; i < 4; i++)
-  {
-    digitalWrite(digitPins[i], HIGH); // Set to HIGH for common cathode
-  }
-}
-
-// Function to display a number on a specific digit
-void displayDigit(int digit, byte segments)
-{
-  clearDigits();                       // Turn off all digits
-  writeShiftRegister(segments);        // Send segment data to the shift register
-  digitalWrite(digitPins[digit], LOW); // Turn on the current digit (set to LOW for common cathode)
-}
 
 void startPauseISR()
 {
@@ -56,14 +66,19 @@ void startPauseISR()
 
 void saveLapISR()
 {
-  Serial.println("Save Lap button pressed");
+
   unsigned long currentTime = millis();
   if ((currentTime - lastDebounceTimeSaveLap) > debounceDelay)
   {
     if (isRunning)
     {
-      lapTimes[lapIndex] = stopwatchTime;
-      lapIndex = (lapIndex + 1) % 4; // Cycle back to 0 after reaching 4
+      lapTimes[lapIndex] = number;
+      lapIndex = (lapIndex + 1) % 4;
+    }
+    else
+    {
+      lapIndex = (lapIndex + 1) % 4;
+      displayNumber = lapTimes[lapIndex];
     }
     // Lap cycling logic will be in the loop
     lastDebounceTimeSaveLap = currentTime;
@@ -72,160 +87,118 @@ void saveLapISR()
 
 void reset()
 {
-
   static unsigned long lastResetButtonPress = 0;
   if (digitalRead(resetButtonPin) == LOW)
-  { // Assuming button connects pin to GND when pressed
+  {
     unsigned long currentTime = millis();
     if (currentTime - lastResetButtonPress > debounceDelay)
     {
       Serial.println("Reset button pressed");
-      if (!isRunning)
+      if (isRunning)
       {
-        stopwatchTime = 0;
-        lastUpdateTime = currentTime;
+        number = 0;
+        displayNumber = 0;
+      }
+      else
+      {
         for (int i = 0; i < 4; ++i)
         {
           lapTimes[i] = 0;
         }
-        lapIndex = 0;
       }
       lastResetButtonPress = currentTime;
     }
   }
 }
 
-// Function to write a byte to the shift register
-void writeShiftRegister(byte data)
-{
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, data);
-  digitalWrite(latchPin, HIGH);
-}
-
-// Placeholder for a function to get the segment values for a digit
-// Segment values for a common anode 7-segment display
-// Assuming segment 'a' is the LSB and 'dp' (decimal point) is the MSB
-const byte segmentValues[] = {
-    0b00111111, // 0
-    0b00000110, // 1
-    0b01011011, // 2
-    0b01001111, // 3
-    0b01100110, // 4
-    0b01101101, // 5
-    0b01111101, // 6
-    0b00000111, // 7
-    0b01111111, // 8
-    0b01101111, // 9
-};
-
-byte getSegmentValue(int digit)
-{
-  if (digit >= 0 && digit <= 9)
-  {
-    return segmentValues[digit];
-  }
-  return 0x00; // Return all segments off if the digit is out of bounds
-}
-void setDigit(int digit, byte segments)
-{
-  // Calculate which 74HC595 outputs to enable for this digit
-  byte digitSelect = 0b00010000 << digit; // This assumes that you have additional outputs for digit selection
-
-  // Send the digit selection followed by the segment values to the shift registers
-  writeShiftRegister(digitSelect);
-  writeShiftRegister(segments);
-
-  // Enable the output (latch the shift register)
-  digitalWrite(latchPin, HIGH);
-}
-
-// Function to update the display
-void updateDisplay()
-{
-  static int currentDigit = 0;
-  byte segments;
-  // Split the stopwatch time into individual digits
-
-  int seconds = (stopwatchTime / 10) % 1000; // Total seconds
-  int tenths = stopwatchTime % 10;           // Tenths of a second
-
-  // Serial.print("Minutes: ");
-  // Serial.print(minutes);
-  // Serial.print(", Seconds: ");
-  // Serial.print(seconds);
-  // Serial.print(", Tenths: ");
-  // Serial.println(tenths);
-  // Convert these digits into the corresponding segments
-  byte digits[numDigits] = {
-      getSegmentValue((seconds / 100) % 10),      // Hundreds place of seconds
-      getSegmentValue((seconds / 10) % 10),       // Tens place of seconds
-      getSegmentValue(seconds % 10) | 0b10000000, // Ones place of seconds, with the decimal point
-      getSegmentValue(tenths)                     // Tenths of a second
-  };
-
-  // Add the decimal point to the second digit (for tenths of a second)
-  digits[2] |= 0b10000000; // Assuming 'dp' is the MSB
-
-  // Update the current digit
-  setDigit(currentDigit, digits[currentDigit]);
-
-  // Display the segments on the current digit
-  // Serial.print("Digit: ");
-  // Serial.print(currentDigit);
-  // Serial.print(", Segments: ");
-  // Serial.println(digits[currentDigit], BIN); // Print in binary format
-  Serial.print(digits[0]);
-  Serial.print("\n");
-  displayDigit(currentDigit, digits[currentDigit]);
-
-  // Move to the next digit
-  currentDigit = (currentDigit + 1) % 4;
-}
-
 void setup()
 {
-  // Button setup
+  // Initialize the pins connected to the shift register as outputs
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  // Initialize digit control pins and set them to LOW (off)
+  for (int i = 0; i < displayCount; i++)
+  {
+    pinMode(displayDigits[i], OUTPUT);
+    digitalWrite(displayDigits[i], HIGH);
+  }
+  digitalWrite(displayDigits[displayCount - 1], LOW);
   pinMode(startPauseButtonPin, INPUT_PULLUP);
   pinMode(resetButtonPin, INPUT_PULLUP);
   pinMode(saveLapButtonPin, INPUT_PULLUP);
 
-  for (int i = 0; i < 4; i++)
-  {
-    pinMode(digitPins[i], OUTPUT);
-  }
-  // Attach the interrupts to the buttons
   attachInterrupt(digitalPinToInterrupt(startPauseButtonPin), startPauseISR, RISING);
   attachInterrupt(digitalPinToInterrupt(saveLapButtonPin), saveLapISR, RISING);
-
-  // Shift register setup
-  pinMode(dataPin, OUTPUT);
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-
-  // Clear the shift register
-  writeShiftRegister(0x00);
-
-  // Serial setup for debugging
+  writeReg(byteEncodings[0]); // Clear the register to avoid ghosting
+  // Begin serial communication for debugging purposes
   Serial.begin(9600);
 }
-
 void loop()
 {
-  // Stopwatch logic
+
   if (isRunning)
   {
-    if (millis() - lastUpdateTime >= updateInterval)
+    if (millis() - lastIncrement > delayCount)
     {
-      stopwatchTime++;
-      lastUpdateTime += updateInterval;
+      number++;
+      displayNumber = number % 10000;
+      lastIncrement = millis();
     }
   }
-
-  // Update the display
-  updateDisplay();
+  writeNumber(displayNumber);
   reset();
+}
+void writeReg(int digit)
+{
+  // Prepare to shift data by setting the latch pin low
+  digitalWrite(latchPin, LOW);
+  // Shift out the byte representing the current digit to the shift register
+  shiftOut(dataPin, clockPin, MSBFIRST, digit);
+  // Latch the data onto the output pins by setting the latch pin high
+  digitalWrite(latchPin, HIGH);
+}
+void activateDisplay(int displayNumber)
+{
+  // Turn off all digit control pins to avoid ghosting
+  for (int i = 0; i < displayCount; i++)
+  {
+    digitalWrite(displayDigits[i], HIGH);
+  }
+  // Turn on the current digit control pin
+  digitalWrite(displayDigits[displayNumber], LOW);
+}
 
-  // Small delay to reduce button bounce
-  delay(5);
+void writeNumber(int number)
+{
+  if (number == 0)
+  {
+    activateDisplay(3);
+    writeReg(byteEncodings[0]);
+    return;
+  }
+  else
+  {
+    int currentNumber = number;
+    int displayDigit = 3;
+    int lastDigit = 0;
+
+    while (currentNumber != 0)
+    {
+      lastDigit = currentNumber % 10;
+      activateDisplay(displayDigit);
+      if (displayDigit == 2)
+      {
+        writeReg((byteEncodings[lastDigit] | B00000001));
+      }
+      else
+      {
+        writeReg(byteEncodings[lastDigit]);
+      }
+      delay(0); // A delay can be increased to visualize multiplexing
+      displayDigit--;
+      currentNumber /= 10;
+      writeReg(B00000000); // Clear the register to avoid ghosting
+    }
+  }
 }
